e6b399c090c10f4bc1446928c27b72101ae126d6
michaltrabski <32506857+michaltrabski@users.noreply.github.com>
2022-12-06 16:32:31 +0100

    1 file modified

--- App.tsx
+++ App.tsx
@@ -45,11 +45,9 @@ export default function App() {
   return (
     <div>
       <h1>note app</h1>
-
       {days.map((day) => {
         return <MyCard day={day} />;
       })}
-
       <Button
         variant="contained"
         size="large"
@@ -58,7 +56,7 @@ export default function App() {
       >
         Load more days...
       </Button>
-
+      działa
       {/* {JSON.stringify(days.slice(0, 1))} */}
     </div>
   );
