1adee42981261dd8a89389a29cf5a8881297fc1e
michaltrabski <32506857+michaltrabski@users.noreply.github.com>
2022-12-06 10:58:48 +0100

    1 file modified

--- App.tsx
+++ App.tsx
@@ -3,29 +3,49 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  const notesData = [
+    {
+      date: '08/12/2022',
+      time: null,
+      title: 'Some example task.',
+      description: 'Lorem ipsum dolor...',
+    },
+  ];
+
   const days = Array.from({ length: 10 }, (_, nr) => {
+    const date = moment().add(nr, 'day');
+    const notes = notesData.filter(
+      (note) => note.date === date.format('DD/MM/YYYY')
+    );
     return {
       nr,
-      date: moment().add(nr, 'day').format('DD/MM/YYYY'),
-      dataName: moment().add(nr, 'day').fromNow(),
-      notes: [],
+      date: date,
+      dataName: date.fromNow(),
+      notesCount: notes.length,
+      notes,
     };
   });
 
   return (
     <div>
       <h1>note app</h1>
-      {JSON.stringify(days)}
 
       <div>
         {days.map((day) => (
-          <div>
+          <div style={{ border: '1px solid grey', marginBottom: '1rem' }}>
             <p>
-              {day.date} {day.dataName}
+              {day.date.format('dddd')} {day.dataName}
             </p>
+            <div>
+              {day.notes.map((note) => (
+                <p>{note.title}</p>
+              ))}
+            </div>
           </div>
         ))}
       </div>
+
+      {JSON.stringify(days)}
     </div>
   );
 }
