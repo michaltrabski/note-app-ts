ec8ab7f0fad2ae9dcfe4fcb99ac7f74477eda5ee
michaltrabski <32506857+michaltrabski@users.noreply.github.com>
2022-12-07 11:55:55 +0100

    added load past days

--- App.tsx
+++ App.tsx
@@ -26,30 +26,42 @@ export interface Day {
 
 export default function App() {
   const [daysLimit, setDaysLimit] = React.useState(10);
+  const [pastDaysLimit, setPastDaysLimit] = React.useState(0);
   const [notesData, setNotesData] = React.useState(() => getNotes());
 
-  const days: Day[] = Array.from({ length: daysLimit }, (_, nr) => {
-    const date = moment().add(nr, 'day');
-    const notes = notesData.filter(
-      (note) => note.date === date.format(DATE_FORMAT)
-    );
+  const days: Day[] = Array.from(
+    { length: daysLimit + pastDaysLimit },
+    (_, nr) => {
+      const date = moment().add(nr - pastDaysLimit, 'day');
+      const notes = notesData.filter(
+        (note) => note.date === date.format(DATE_FORMAT)
+      );
 
-    return {
-      nr,
-      date: date,
-      dataName: date.fromNow(),
-      notesCount: notes.length,
-      notes,
-    };
-  });
+      return {
+        nr,
+        date: date,
+        dataName: date.fromNow(),
+        notesCount: notes.length,
+        notes,
+      };
+    }
+  );
 
   return (
     <div>
       <h1>note app</h1>
+      <Button
+        sx={{ marginBottom: 2 }}
+        variant="contained"
+        size="small"
+        fullWidth
+        onClick={() => setPastDaysLimit((prevLimit) => prevLimit + 3)}
+      >
+        Load more past days...
+      </Button>
       {days.map((day) => {
         return <MyCard day={day} />;
       })}
-      
       <Button
         variant="contained"
         size="large"
@@ -58,7 +70,6 @@ export default function App() {
       >
         Load more days...
       </Button>
-      działa3
       {/* {JSON.stringify(days.slice(0, 1))} */}
     </div>
   );
